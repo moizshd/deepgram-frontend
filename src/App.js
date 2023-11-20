2ac478db7d287847d86842e0ed9acb4efdff06ee
Moiz Shahid <82671962+moizshd@users.noreply.github.com>
2023-11-20 12:42:09 +0500

    updated app.js
    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 function App() {
   const [transcription, setTranscription] = useState('');
+  const [partiialTranscription, setPartialTranscription] = useState('');
+  const lastFinalTranscriptRef = useRef('');
+
   const [isRecording, setIsRecording] = useState(false);
   const [ws, setWs] = useState(null);
-  let mediaRecorder;
+  const mediaRecorderRef = useRef(null);
 
-  useEffect(() => {
-    function connectWebSocket() {
-      const newWs = new WebSocket('ws://localhost:3001');
+  const connectWebSocket = () => {
+    const newWs = new WebSocket('ws://localhost:3001');
+
 
       newWs.onopen = () => {
         console.log("WebSocket connection established");
@@ -25,27 +28,50 @@ function App() {
       };
       newWs.onmessage = (event) => {
         const received = JSON.parse(event.data);
-        const { is_final } = received
-        const transcript = received.channel.alternatives[0].transcript
-        if (is_final && transcript) {
-          console.log("Received:", received);
 
-          if (received.type === "Results" && received.channel.alternatives.length > 0) {
-            const newTranscript = received.channel.alternatives[0].transcript;
-            console.log("New Transcript:", newTranscript);
+        // Check if the received message has the expected structure
+        if (received && received.channel && received.channel.alternatives && received.channel.alternatives.length > 0) {
+          const { is_final, speech_final } = received;
+          const transcript = received.channel.alternatives[0].transcript;
 
-            if (newTranscript) {
-              setTranscription(prevTranscript => prevTranscript + newTranscript + " ");
-            }
+        console.log("transcript", transcript, "isfinal", is_final, "speechFinal", speech_final)
+        if (transcript) {
+          if (is_final) {
+            setPartialTranscription('');
+            console.log("1")
+            lastFinalTranscriptRef.current += transcript + " ";
+          }
+          if (speech_final) {
+            console.log("2")
+
+            setTranscription(lastFinalTranscriptRef.current + " ");
+            // Reset lastFinalTranscriptRef and partialTranscriptionRef if needed
+          }
+          if (!is_final && !speech_final) {
+            console.log("3")
+
+            setPartialTranscription(transcript);
           }
         }
+        else if (!transcript && speech_final) {
+          console.log("4")
+
+          setTranscription(lastFinalTranscriptRef.current + " ");
+          // Reset lastFinalTranscriptRef and partialTranscriptionRef if needed
+        }
+
+        // Force update to re-render the component
+        // This is a workaround to trigger re-render when useRef values are updated
+        setIsRecording(isRecording => !isRecording);
+        setIsRecording(isRecording => !isRecording);
       };
 
+    }
 
 
       setWs(newWs);
     }
-
+  useEffect(() => {
     connectWebSocket();
     return () => {
       if (ws) {
@@ -53,10 +79,20 @@ function App() {
       }
     };
   }, []);
+    
 
+
+  // useEffect(() => {
+  //   console.log("My Last Final transcription in UE", lastFinalTranscript)
+  // }, [lastFinalTranscript])
 
   const startRecording = () => {
     console.log("Attempting to start recording...");
+
+    // Re-open WebSocket connection if it's closed
+    if (!ws || ws.readyState === WebSocket.CLOSED) {
+      connectWebSocket();
+    }
 
     navigator.mediaDevices.getUserMedia({ audio: true })
       .then(stream => {
@@ -66,15 +102,15 @@ function App() {
           return;
         }
 
-        mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/webm' });
-        mediaRecorder.onstart = () => console.log("MediaRecorder started");
-        mediaRecorder.onerror = (error) => console.error("MediaRecorder error:", error);
-        mediaRecorder.start(1000); // Experiment with different timeslice values
+        mediaRecorderRef.current = new MediaRecorder(stream, { mimeType: 'audio/webm' });
+        mediaRecorderRef.current.onstart = () => console.log("MediaRecorder started");
+        mediaRecorderRef.current.onerror = (error) => console.error("MediaRecorder error:", error);
+        mediaRecorderRef.current.start(1000); // Experiment with different timeslice values
 
 
-        mediaRecorder.addEventListener("dataavailable", event => {
+        mediaRecorderRef.current.addEventListener("dataavailable", event => {
           if (ws && ws.readyState === WebSocket.OPEN) {
-            console.log("Sending audio data to server, size:", event.data.size);
+            // console.log("Sending audio data to server, size:", event.data.size);
             ws.send(event.data);
           } else {
             console.log("WebSocket not ready or closed");
@@ -87,11 +123,23 @@ function App() {
       });
   };
 
-
   const stopRecording = () => {
-    mediaRecorder.stop();
-    setIsRecording(false);
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+      setIsRecording(false);
+
+      // Close the WebSocket connection when stopping transcription
+      if (ws) {
+        ws.close();
+        setWs(null); // Reset WebSocket state
+      }
+    } else {
+      console.error('MediaRecorder not initialized');
+    }
   };
+
+
+
 
   return (
     <div>
@@ -99,7 +147,12 @@ function App() {
       <button onClick={isRecording ? stopRecording : startRecording}>
         {isRecording ? 'Stop Recording' : 'Start Recording'}
       </button>
-      <p>{transcription}</p>
+      {/* Display Partial Transcript in Grey */}
+      <p style={{ color: 'grey' }}>THIS IS PARTIAL TRANSCRIPTION : {partiialTranscription}</p>
+      <p style={{ color: 'red' }}>THIS IS SEMI PARTIAL TRANSCRIPTION : {lastFinalTranscriptRef.current}</p>
+
+      {/* Display Final Transcript in Black */}
+      <p style={{ color: 'black' }}>THIS IS FINAL TRANSCRIPTION : {transcription}</p>
     </div>
   );
 }
